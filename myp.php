44597cc4afc0e6e285adb7bfd2cd104693cec738
mamlashi <47587256+mamlashi@users.noreply.github.com>
2019-03-30 20:49:49 -0700

    Update myp.php

--- myp.php
+++ myp.php
@@ -1,14 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "Matt";
-$password = "ms";
-$database="Staging";
-$myname=$_POST['name'];
-$mynum=$_POST['mynum'];
-$myid=$_POST['myid'];
-// Create connection
+$username = "mamlashi";
+$password = "ms013000";
 $conn = new mysqli($servername, $username, $password,$database);
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
